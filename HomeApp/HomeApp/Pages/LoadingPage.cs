83c716a5ec1babae5c4ed3df31a9422fd2d27670
DiMatveev1991 <98545906+DiMatveev1991@users.noreply.github.com>
2024-03-09 21:50:38 +0300

    HM

---
+++ HomeApp/HomeApp/Pages/LoadingPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HomeApp.Pages
+{
+    class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            // Объявим новый текстовый элемент
+            Label header = new Label() { Text = $"Загрузка моего 1-го приложения{Environment.NewLine} на Xamarin... для Гришки!!!! " };
+
+            // Здесь можно сразу установить стили и шрифт
+            header.Opacity = 0;
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+            header.FontSize = 21;
+            // Можем даже задать анимацию
+            header.FadeTo(1, 5000);
+
+            // Инициализация свойства Content новым элементом.
+            Content = header;
+
+            //header.Text = $"Приложени загружено...";
+
+
+        }
+    }
+}
---
+++ HomeApp/HomeApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {   
+        // Константs для текста кнопки
+        public const string BUTTON_TEXT = "Войти";
+        public const string PIN_TEXT_ENTRY = "Введите пин-код для входа:";
+        public const string PIN_TEXT_ERROR = "Неверный ПИН-код";
+        public const string PIN_TEXT_LEN_ERROR = " символа требуется в ПИН-коде";
+        public const int PIN_LEN = 4;
+        private string _pin; //4321
+        // Переменная счетчика
+        public static int loginCounter = 0;
+       
+        public LoginPage()
+        {
+            InitializeComponent();
+            _pin = Preferences.Get("Password", String.Empty);
+            if (_pin != string.Empty)
+            {
+                lgMessage.Text = PIN_TEXT_ENTRY;                
+            }
+        }
+        //private void Login_Click(object sender, EventArgs e)
+        //{
+        //    loginButton.Text = "Выполняется вход d ...";
+        //    //string xaml = "<Button Text=\"⌛ Выполняется вход..\"  />";
+        //    //loginButton.LoadFromXaml(xaml);
+        //}
+        /// <summary>
+        /// По клику обрабатываем счётчик и выводим разные сообщения
+        /// </summary>
+        //private void Login_Click(object sender, EventArgs e)
+        //{
+        //    if (loginCounter == 0)
+        //    {
+        //        // Если первая попытка - просто меняем сообщения
+        //        loginButton.Text = $"Выполняется вход..";
+        //    }
+        //    else if (loginCounter > 5) // Слишком много попыток - показываем ошибку
+        //    {
+        //        // Деактивируем кнопку
+        //        loginButton.IsEnabled = false;
+        //        // Показываем текстовое сообщение об ошибке
+        //        lgMessage.Text = $"Слишком много попыток! Попробуйте позже. Попыток входа: {loginCounter}";
+        //    }
+        //    else
+        //    {
+        //        // Изменяем текст кнопки и показываем количество попыток входа
+        //        loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCounter}";
+        //    }
+
+        //    // Увеличиваем счетчик
+        //    loginCounter += 1;
+        //}
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            string enterPwd = Password.Text;
+            if (_pin == string.Empty)
+            {
+                Preferences.Set("Password", enterPwd);
+            }
+            else
+            {
+                if (_pin != Password.Text)
+                {
+                    string sTxt = PIN_TEXT_ERROR;
+                    sTxt.Concat(PIN_LEN.ToString()) ;
+                    lgMessage.Text = sTxt;
+                    return;
+                }
+            }
+
+            Navigation.PushAsync(new GalleryPage());
+        }
+
+        private void Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Password.Text.Length != PIN_LEN)
+            {
+                lgMessage.Text = PIN_TEXT_LEN_ERROR;
+                loginButton.IsEnabled = false;
+            }
+            else
+            {
+                loginButton.IsEnabled = true;
+                lgMessage.Text = string.Empty;
+            }
+        }
+    }
+}
